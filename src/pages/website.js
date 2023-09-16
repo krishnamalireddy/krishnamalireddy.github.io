c2ba8f6a310fc6f2c380b8acfd2777e26f2e3fe1
Krishnamalireddy <krishnamalireddy7@gmail.com>
2023-09-16 20:38:19 +0530

    changed conetnt

--- src/pages/website.js
+++ src/pages/website.js
@@ -19,10 +19,9 @@ const ProfileApp = () => {
                           Front End Developer
                         </p>
                         <h2 className="font-bold text-[2.5rem] md:text-[3rem] mb-2">Krishna Chaitanya Malireddy</h2>
-                        <p className="text-[1rem] text-base	 mb-10"> Hi, I'm having my bread and butter in Bengaluru.
-                            Developer by day, Vampire Slayer by night
-                            Besides having a full time carrier in IT industry, I love travelling and discussing about Share market.
-                        </p>
+                        <p className="text-[1rem] text-base	 mb-10"> 
+
+                          I hail from the serene shores of Kakinada, but my current journey has led me to Bengaluru, the city that never sleeps, where I earn a living as dedicated software developer during the day and engage in vampire-slaying adventures at night. In addition to my full-time IT career, I also enjoy travelling, Playing Sports and having intriguing discussions about the stock market.                        </p>
                             <div className="profile_buttons">
                               <a className="px-8 py-3 border border-white-500 border-solid bg-transparent hover:bg-orange-700 rounded" href={process.env.PUBLIC_URL + '/Krishna_resume.pdf'} target="_blank"> Download Resume</a>
                             </div>
@@ -40,12 +39,12 @@ const ProfileApp = () => {
                     </div>
                     <div className="profile_banner_head content-center	 md:flex items-center columns-2xs justify-around	align-middle ">
                         <div className="md:w-1/2  md:mr-20  text-base my-6">
-                        I'm a Frontend Web Developer building the Front-end of Websites and Web Applications that leads to the success of the overall product. Don’t ask me about my graduation because I don’t know why I have done it. 
-                        After completion of  my Bachelor's degree, I initially joined my family's business but soon realized it wasn't my true calling. I decided to pursue my passion for technology, relocated to a new city and dived into software courses. This journey led me to first job as a Software Developer at a startup, where I wore multiple hats, including Database Administrator and Full-stack Developer. My experiences with languages like JAVA, SQL, and PL-SQL, along with advanced web development, fueled my passion for software technologies.
-                        <br /> <br />
-                        I'm dedicated to staying at the forefront of this ever-evolving field and exploring emerging technologies."
+                         I mainly work with div's, Styles and Scripts. Passionate about Promoting Brands through my work, weaving stories through blocks and colours, inspired by feelings, dreams and technology. My art intends to represent strong confident characters of Organizations.
                         <br /><br />
-                        I'm open to Job opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience then don't hesitate to contact me.
+                        I work closely with talented content writers and designers as part of my Work. Together, we weave a tapestry of art one pixel at a time where their words and designs come to life through my development and deployment. It's an amalgamation of their creativity and my technical expertise, resulting in a harmonious blend of design, content, and functionality.
+                        <br /> <br />
+                        I'm open to Job opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience then don't hesitate to <a href="mailto:krishna5@duck.com">Contact Me</a>
+
                         </div>
                         <div className=" md:w-1/2  md: md:ml-20 ">
                             <Lottie animationData={dev_profile_lottie}  className="max-w-md relative m-auto"/>
@@ -59,12 +58,12 @@ const ProfileApp = () => {
                 <div className="box-main">
                     <div className="">
                     <h1 className=" text-[2.5rem] sm:text-[4rem] py-6" id="program">
-                    Languages I speak
+                    Mastering the Melody of Code
                         </h1>
                     </div>
                     <div className="profile_banner_head content-center	md:flex-row-reverse md:flex items-center columns-2xs justify-around	align-middle ">
                         <div className=" md:w-1/2  md:ml-20">
-                            <p>"I am fluent in several programming languages, including HTML, CSS, JavaScript, Jquery, PHP and React." These languages are the fundamentals of contemporary web development, and they enable me to make interactive and user-friendly websites.
+                            <p>"I am fluent in several languages, including HTML, CSS, JavaScript, Jquery, PHP and React." These languages are the fundamentals of contemporary web development, and they enable me to make interactive and user-friendly websites.
                             <br /> <br />In addition to coding languages, I am proficient in the use of essential frameworks such as Bootstrap, Sass for efficient CSS preprocessing and Tailwind CSS for streamlined styling. I have experience using GitHub for version control, Figma for design collaboration, WordPress for content management, and Visual Studio Code as my preferred integrated development environment. With the help of these tools, I can quickly and effectively deliver high-quality web solutions, ensuring a smooth development process from conception to deployment.
                             </p>
                         </div>
